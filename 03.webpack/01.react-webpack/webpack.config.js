2f9da6eea957fecf6c7af59a697491aff0d6d5d8
Wilder Lizama Garcia <wilderlizama@gmail.com>
2019-01-08 09:49:20 -0500

    webpack - clean webpack plugin

--- 03.webpack/01.react-webpack/webpack.config.js
+++ 03.webpack/01.react-webpack/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle-[hash].js"
+        filename: "bundle-[hash:8].js"
     },
     module: {
         rules: [
@@ -21,6 +22,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html"
-        })
+        }),
+        new CleanWebpackPlugin(["./dist"])
     ]
 }
