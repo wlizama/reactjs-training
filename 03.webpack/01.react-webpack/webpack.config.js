e7dd8da84776e32c295c0f4563c627e918cf8cb5
Wilder Lizama Garcia <wilderlizama@gmail.com>
2019-01-11 09:56:27 -0500

    webpack - se agrega webpack configurado para producción en archivos CSS

--- 03.webpack/01.react-webpack/webpack.config.js
+++ 03.webpack/01.react-webpack/webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const CleanWebpackPlugin = require("clean-webpack-plugin")
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const webpack = require("webpack")
 
 module.exports = {
@@ -36,13 +37,12 @@ module.exports = {
             new UglifyJsPlugin({
                 parallel: true,
                 extractComments: true,
-            })
+            }),
+            new OptimizeCSSAssetsPlugin()
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        }),
+        new HtmlWebpackPlugin({ template: "./src/index.html" }),
         new CleanWebpackPlugin(["./dist"]),
         new MiniCssExtractPlugin({ filename: "[name]-[hash:8].css" }),
         new webpack.HotModuleReplacementPlugin()
