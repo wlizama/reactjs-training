0d29878bdd784b4a9e414e53d2f45d8787c88db8
Wilder Lizama Garcia <wilderlizama@gmail.com>
2019-02-05 17:49:21 -0500

    Rutas para usuarios

--- 04.react-router/app-router/src/Routes.js
+++ 04.react-router/app-router/src/Routes.js
@@ -1,13 +1,21 @@
 import { Home } from "./components/Home"
 import { About } from "./components/About"
 import { Users } from "./components/Users"
+import { User } from "./components/User"
 import { ContactUs } from "./components/ContactUs"
 import { NoMatch } from "./components/NoMatch"
 
 let Routes = [
     { path: "/", title: "Home", component: Home, exact: true },
     { path: "/about", title: "About", component: About },
-    { path: "/users", title: "Users", component: Users },
+    {
+        path: "/users",
+        title: "Users",
+        component: Users, 
+        routes: [
+            { path: "/users/:id", title: "User", component: User }
+        ]
+    },
     { path: "/contact", title: "Contact Us", component: ContactUs },
     { notLink: true, component: NoMatch },
 ]
