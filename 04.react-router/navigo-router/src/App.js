f8f40faf091b2bf07b0f739afd3bda0a2ea1675e
Wilder Lizama Garcia <wilderlizama@gmail.com>
2019-02-18 17:10:52 -0500

    init navigo router training

---
+++ 04.react-router/navigo-router/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <header>Navigo Router</header>
+        <section>--content here--</section>
+      </>
+    );
+  }
+}
+
+export default App;
