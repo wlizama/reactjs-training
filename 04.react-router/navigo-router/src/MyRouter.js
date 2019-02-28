92a04a24bdfa77b29589ac33959c8e0dc4cf33e3
Wilder Lizama Garcia <wilderlizama@gmail.com>
2019-02-28 12:13:01 -0500

    navigo-router no con reactjs

--- 04.react-router/navigo-router/src/MyRouter.js
+++ 04.react-router/navigo-router/src/MyRouter.js
@@ -6,20 +6,26 @@ import About from "./components/About"
 let router_root = null
 let useHash = false
 let hash = "#!"
-let router = new Navigo(router_root, useHash, hash)
+let router = new Navigo(router_root, useHash)
 
 router.on({
     "about": () => <About />
-})
+}).resolve()
 
 class MyRouter extends React.Component {
     
     constructor(props) {
         super(props)
     }
+    componentDidMount() {
+        // router.navigate("/about")
+    }
     render() {
         return (
-            <h2>About ..., parece que no funcionara </h2>
+            <div>
+                <h2>Navigo Router Components</h2>
+                <a href="about" data-navigo>About</a>
+            </div>
         )
     }
 }
