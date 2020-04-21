c4fe2e40981d56cca71ba4146f99abfb0110f2de
Wilder Lizama Garcia <wilderlizama@gmail.com>
2020-04-20 22:09:46 -0500

    init MUI testing

---
+++ 05.mui/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+
+const App = () => {
+	return (
+		<div>
+			<Button color='primary'>Hello MUI</Button>
+		</div>
+	)
+}
+
+export default App
